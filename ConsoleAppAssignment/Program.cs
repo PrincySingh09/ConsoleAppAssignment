628e8a38928a0ac731b3952b20fba37e8e257890
princysingh2409@gmail.com <princysingh2409@gmail.com>
2023-07-17 16:46:51 +0530

    Add project files.

---
+++ ConsoleAppAssignment/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            try
+            {
+                User user = new User();
+                Console.WriteLine("Enter your name:");
+                user.Name = Console.ReadLine();
+                if (user.Name.Length < 6)
+                {
+                    throw new ValidationException("Name should be atleast 6 characters long.");
+                }
+                Console.WriteLine("Enter your email:");
+                user.Email = Console.ReadLine();
+                if (!user.Email.Contains('@') || !user.Email.Contains('.'))
+                {
+                    throw new ValidationException("Email is not valid. It must contain an '@' and a '.' character.");
+                }
+                Console.WriteLine("Enter your password:");
+                user.Password = Console.ReadLine();
+                if (user.Password.Length < 6)
+                {
+                    throw new ValidationException("Password should be atleast 6 characters long.");
+                }
+                Console.WriteLine("Registration successful!!");
+            } catch (ValidationException ex)
+            {
+                Console.WriteLine("Error: "+ ex.Message);
+            }
+              
+            }
+        }
+    }
+
---
+++ ConsoleAppAssignment/User.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleAppAssignment
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+    }
+    public class ValidationException : Exception
+    {
+        public ValidationException(string message): base(message)  { }
+    }
+}
